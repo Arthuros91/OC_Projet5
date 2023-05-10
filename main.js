f251816cafd3af958ad839ba7a4a084e17942f93
Arthuros <arthur-a54@hotmail.fr>
2023-05-10 02:37:20 +0200

    boostrap and jquery deleted, W3C erros fixed, semantic error fixed, gallery (in progress), caroussel (in progress)

---
+++ main.js
@@ -0,0 +1,13 @@
+const gallery = $(".gallery");
+
+
+
+/* BUTTONS FUNCTIONS */
+
+const btnAll = $(".btnAll");
+const btnConcerts = $(".btnConcerts");
+const btnEntreprises = $(".btnEntreprises");
+const btnMariages = $(".btnMariages");
+const btnPortraits = $(".btnPortraits");
+
+

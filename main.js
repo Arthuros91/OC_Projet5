2fda6569c3ad6792b2d05fde507b6803dd188f16
Arthuros <arthur-a54@hotmail.fr>
2023-05-11 21:15:48 +0200

    header fixed + navigations buttons fixed + animation (need to fix the position of buttons)

--- main.js
+++ main.js
@@ -10,4 +10,48 @@ const btnEntreprises = $(".btnEntreprises");
 const btnMariages = $(".btnMariages");
 const btnPortraits = $(".btnPortraits");
 
+const leftButton = $(".leftArrow");
+const rightButton = $(".rightArrow");
 
+const carousel = $(".carousel-inner");
+const carousel_img = $(".carousel-inner img");
+
+const imgSize = carousel_img.width();
+const maxSize = (carousel.children().length - 1) * imgSize;
+
+
+var scrollInterval;
+var scrollValue = 0;
+const header = $("header");
+
+carousel.css("margin-top", header.height());
+navigateButtons();
+playScrollAnimation();
+
+
+function navigateButtons(){
+    rightButton.on("click", function(){ 
+        scrollValue += imgSize ;
+        if (scrollValue > maxSize) { 
+            scrollValue = 0;
+        }
+        carousel.animate({ scrollLeft: scrollValue }, 0);  
+    }); 
+    leftButton.on("click", function(){ 
+        scrollValue -=imgSize;
+        if (scrollValue < 0) { 
+            scrollValue = maxSize;
+        }
+        carousel.animate({ scrollLeft: scrollValue }, 0);
+    });
+};
+
+function playScrollAnimation() {
+    scrollInterval = setInterval(function(){
+        scrollValue += carousel_img.width(); 
+        if (scrollValue > maxSize) { 
+            scrollValue = 0;
+        }
+        carousel.animate({ scrollLeft: scrollValue }, 0); 
+    }, 5000); 
+}
